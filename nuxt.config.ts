0478950a4ac9d3f159f1daf20c187d0e67cc2503
Kevin McCarthy <kjmagic13@gmail.com>
2023-03-13 17:59:44 -0400

    cleanup from merge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,7 @@ export default defineNuxtConfig({
   css: [
     //
     '~~/assets/styles.scss',
-    // //
-    // '@fortawesome/fontawesome-free/css/fontawesome.css',
-    // '@fortawesome/fontawesome-free/css/brands.css',
-    // '@fortawesome/fontawesome-free/css/solid.css',
   ],
-
-  // css: ['~~/assets/vuetify-custom.scss'],
 
   build: {
     transpile: ['vuetify'],
